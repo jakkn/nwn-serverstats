bae9a6e6e8ae7df62fcf7defab48fa8704ca0e70
Jakob Knutsen <jakobknutsen@gmail.com>
2015-12-26 18:11:57 +0100

    Load a chart
    Dummy chart with static data.

--- src/app.js
+++ src/app.js
@@ -3,15 +3,8 @@ var express = require('express');
 // Constants
 var PORT = 3000;
 
-// App
 var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello world\nWe made it!');
-});
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+app.use(express.static('public'));
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+app.listen(process.env.PORT || PORT);
